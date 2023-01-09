19c19fd09274f07820f619b024268849a9083bd7
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-09 17:11:15 -0300

    .NET 7 + ASP.NET Core + API REST (Contagem de Acessos) + Rate Limiting (Fixed Window) + Dockerfile + Linux
    .NET 7 + ASP.NET Core + API REST (Contagem de Acessos) + Rate Limiting (Fixed Window) + Dockerfile + Linux

---
+++ APIContagem/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(_ => _
+    .AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 3;
+        options.Window = TimeSpan.FromSeconds(5);
+    }));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseRateLimiter();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
